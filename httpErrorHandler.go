a7a86904edf2d69abb5210880e7e755ebcc3ee94
Steven Frew <steven.fullhouse@gmail.com>
2022-01-19 15:56:28 +0000

    fixed wrong name

--- httpErrorHandler.go
+++ httpErrorHandler.go
@@ -19,9 +19,9 @@ type Error struct {
 // Handler is a function that handles an HTTP error, probably by writing the error details to the response.
 type Handler func(w http.ResponseWriter, r *http.Request, httpError *Error)
 
-// HandleErrors makes a call to the given hander function, and, in the event of an HTTP server error result, calls your
+// Handle makes a call to the given hander function, and, in the event of an HTTP server error result, calls your
 // supplied error handler function.
-func handle(w http.ResponseWriter, r *http.Request, handlerFunc func(w http.ResponseWriter, r *http.Request) *Error, errorHandlerFunc Handler) {
+func Handle(w http.ResponseWriter, r *http.Request, handlerFunc func(w http.ResponseWriter, r *http.Request) *Error, errorHandlerFunc Handler) {
 	err := handlerFunc(w, r)
 	// If we get back an error from the handlerFunc, write it to the response and set the appropriate status.
 	if err != nil {
